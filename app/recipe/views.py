0d0211ac5a8b85fc469716d0ad18da637803f64a
pranav <pranav.chandran@gmail.com>
2021-07-03 13:41:11 +0530

    Added feature for dilter recipes by tags and ingredients

--- app/recipe/views.py
+++ app/recipe/views.py
@@ -53,8 +53,8 @@ class RecipeViewSet(viewsets.ModelViewSet):
     authentication_classes = (TokenAuthentication,)
     permission_classes = (IsAuthenticated,)
 
-    def _params_to_ints(self, qs):
-        """Convert a list of string IDs to a list of integers"""
+    def _params_to_int(self, qs):
+        """Convert a lost of string IDs to a list of integers"""
         return [int(str_id) for str_id in qs.split(',')]
 
     def get_queryset(self):
@@ -63,12 +63,11 @@ class RecipeViewSet(viewsets.ModelViewSet):
         ingredients = self.request.query_params.get('ingredients')
         queryset = self.queryset
         if tags:
-            tag_ids = self._params_to_ints(tags)
+            tag_ids = self._params_to_int(tags)
             queryset = queryset.filter(tags__id__in=tag_ids)
         if ingredients:
-            ingredient_ids = self._params_to_ints(ingredients)
+            ingredient_ids = self._params_to_int(ingredients)
             queryset = queryset.filter(ingredients__id__in=ingredient_ids)
-
         return queryset.filter(user=self.request.user)
 
     def get_serializer_class(self):
